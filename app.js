58304f6ea94536c2c92d4e1b5042891eb7c32182
Daniel Dìaz <danieldiazc413@gmail.com>
2023-11-22 16:58:58 -0500

    inicializacion sockets

--- app.js
+++ app.js
@@ -1,14 +1,46 @@
-const express = require('express');
-const app = express();
-const http = require('http');
-const server = http.createServer(app);
 const { Server } = require("socket.io");
-const io = new Server(server);
 
-io.on('connection', (socket) => {
-  console.log('a user connected');
+const io = new Server({ 
+  cors: {
+    origin: "http://localhost:3002",
+    methods: ["GET", "POST"]
+  }
 });
 
-server.listen(5050, () => {
-  console.log('listening on *:5050');
+
+io.on("connection", (socket) => {
+  const { idUsuario, tipoUsuario } = socket.handshake.query;
+  // console.log(socket.handshake.query)
+
+  if(tipoUsuario === 'CONDUCTOR'){
+    console.log(`Conductor conectado ${idUsuario}`);
+    socket.join("CONDUCTORES");
+
+    //emit to conductores room a message
+    io.to("CONDUCTORES").emit("message", "Otro conductor se ha conectado");
+    
+  }
+
+  if(tipoUsuario === 'CLIENTE'){
+    console.log(`Conductor conectado ${idUsuario}`);
+    socket.join("CLIENTES");
+  }
+
+  
+  // Manejar el evento disconnect
+  socket.on("disconnect", () => {
+    console.log('usuario desconectado: ', idUsuario, ' tipo: ', tipoUsuario);
+    // Puedes realizar acciones adicionales cuando un usuario se desconecta
+  });
+  
+
+  
+  
 });
+
+// io.on('disconnect', () => {
+//   console.log('Conductor desconectado');
+// });
+
+
+io.listen(5050);
